1ff3f1f1f9d5a1018c2c68af7b4622bbe30df033
Mariana <marianapenarandanazareth@gmial.com>
2022-05-04 10:24:10 -0500

    Evaluacioncondicionles.js

---
+++ taller4.js
@@ -0,0 +1,102 @@
+// 1 
+var saludar = "si"
+if (saludar == "si"){
+console.log ("Hola soy un condicional simple"); 
+}
+
+// 2
+var x=20;
+ 
+if(x<=20){
+    console.log("La condición es verdadera");
+}
+var edad = 20 
+console.log ("Lo siento,no tiene edad suficiente para entrar aqui");
+
+// 3 
+var edad = 18
+ 
+if(edad>=18){	
+    console.log("Bienvenido, puede ingresar");
+}else{
+    console.log("Lo siento, no tiene edad suficiente para entrar aquí");
+}
+
+// 4
+var numero1 = 5;
+var numero2 = 8;
+ 
+if(numero1<numero2) {
+  console.log("numero1 no es mayor que numero2");
+}
+if(numero2>0) {
+  console.log("numero2 es positivo");
+}
+if(numero1<0) {
+  console.log("numero1 es negativo o distinto de cero");
+}
+if(numero1<numero2) {
+  console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
+
+// 5 
+var numerito = 90;
+ 
+if(numerito<50) {
+  console.log("numerito sí es estrictamente menor que 50");
+}else if(numerito>50 && numerito<88) {
+  console.log("numerito sí está entre 50 y 80");
+}else if(numerito>80) {
+  console.log("numerito es mayor que 80 pero no mayor ni igual a 100");
+}else if(numerito == 100) {
+  console.log("numerito es igual a 100");
+}else{
+    console.log("numerito es estrictamente mayor a 100");
+}
+
+
+//6
+numerito = 90;
+//7
+numerito = 95;
+// 8
+numerito = 101; 
+
+// 9 
+var dia= "jueves"
+if (dia == "jueves"){
+console.log ("Tiene permitido ir al polideportivo")
+}else if (dia != "jueves"){ 
+console.log ("No tiene permitido ir al polideortivo")
+}
+
+// 10 
+var numero1 = 4
+var numero2 = 0
+if (numero2 == 0){ 
+    console.log ("No se puede operar con el 0")
+}
+else {console.log ("El resultado de la opertacion es " + numero1/numero2 )
+}
+
+// 11
+var diaimportante = "miercoles"
+if (diaimportante == "lunes"){
+console.log ("el plato del dia es arroz con pollo")
+}
+else {console.log ("el plato del dia es chuleta de cerdo/pollo")
+}
+if (diaimportante == "lunes"){
+console.log ("el plato del dia es arroz con pollo")
+    }
+if (diaimportante == "lunes"){
+console.log ("el plato del dia es arroz con pollo")
+        }
+ if (diaimportante == "lunes"){
+console.log ("el plato del dia es arroz con pollo")
+            }
+if (diaimportante == "lunes"){
+console.log ("el plato del dia es arroz con pollo")
+                }if (diaimportante == "lunes"){
+console.log ("el plato del dia es arroz con pollo")
+}
